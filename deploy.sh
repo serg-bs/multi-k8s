3ad45bf98d8a3c625f2c3b2d0ce212520ef4909e
Беспалов Сергей Михайлович <bespalov@srvhub.ru>
2019-12-19 16:20:04 +0300

    change credential

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t serg-bs/multi-client:latest -t serg-bs/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t serg-bs/multi-server:latest -t serg-bs/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t serg-bs/multi-worker:latest -t serg-bs/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push serg-bs/multi-client:latest
+docker push serg-bs/multi-server:latest
+docker push serg-bs/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push serg-bs/multi-client:$SHA
+docker push serg-bs/multi-server:$SHA
+docker push serg-bs/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=serg-bs/multi-server:$SHA
+kubectl set image deployments/client-deployment client=serg-bs/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=serg-bs/multi-worker:$SHA
